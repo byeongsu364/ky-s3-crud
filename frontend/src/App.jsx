ad42d4d098f522be11fc916241248ecd7f979ffd
byeongsu364 <kbsyjysbs@gmail.com>
2025-09-16 21:34:58 +0900

    ddd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import FileList from "./components/FileList"
+import UploadForm from "./components/UploadForm"
+
+function App() {
+
+  return (
+    <div className='container'>
+      <UploadForm/>
+      <FileList/>
+    </div>
+  )
+}
+
+export default App
